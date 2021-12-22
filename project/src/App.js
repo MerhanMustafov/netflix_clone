78048a6aeeeed3acf5e4169313548e8ed3290b91
unknown <m.o.mustafov@gmail.com>
2021-12-22 17:37:01 +0200

    commit

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import Row from './components/Row.js'
-import requests from './requests.js'
+import React from "react";
+import "./App.css";
+import Row from "./components/Row.js";
+import requests from "./requests.js";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
-    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="TOP RATED" fetchUrl={requests.fetchTrending}/>
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="TRENDINGS" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+      <Row title="ACTION" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- project/src/components/Row.js
+++ project/src/components/Row.js
@@ -1,32 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import axios from '../axios.js'
+import React, { useState, useEffect } from "react";
+import axios from "../axios.js";
+import "../css/Row.css";
 
-const baseUrl = 'https://image.tmdb.org/t/p/original/'
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([])
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData(){
-            const request = await axios.get(fetchUrl) //====> 'https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US'
-            setMovies(request.data.results)
-            return request;
-        }
-        fetchData()
-    }, [fetchUrl]) 
-    console.table(movies)
-    return (
-        <div className="row">
-            <h2>{title}</h2>
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl); //====> 'https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US'
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  // console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
 
-            <div className="row_posters">
-                {movies.map(movie => <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />)}
-            </div>
-        </div>
-    )
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && `row_poster_large`}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
-
-
-
+export default Row;
