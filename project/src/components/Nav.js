b248d75fc6c3a20da143c2df4eae559d3a361519
unknown <m.o.mustafov@gmail.com>
2021-12-23 16:44:07 +0200

    modified nav bar

--- project/src/components/Nav.js
+++ project/src/components/Nav.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import netflix_Logo from "../images/netflix_Logo.jpg";
+import "../css/Nav.css";
 
 function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
-      <img
-        className="nav_log"
-        src="images/netflix_logo.jpg"
-        alt="Netflix Logo"
-      />
+    <div className={`nav ${show && "nav_bar_black"}`}>
+      <img className="nav_log" src={netflix_Logo} alt="Netflix Logo" />
     </div>
   );
 }
